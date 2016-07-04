03242b3091c19e9a626135f9bc34f62cce3d585b
FilipVanVooren <filip.van-vooren@oratronik.de>
2016-07-04 22:00:52 +0200

    Firefox browser add-on stuff for getting through the review

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-var gulp   = require('gulp');
-var concat = require('gulp-concat');
-var header = require('gulp-header');
-var jshint = require('gulp-jshint');
-var df     = require('dateFormat');
+var gulp            = require('gulp');
+var concat          = require('gulp-concat');
+var header          = require('gulp-header');
+var jshint          = require('gulp-jshint');
+var df              = require('dateFormat');
+var strip_comments  = require('gulp-strip-comments');
+var strip_debug     = require('gulp-strip-debug');
 
 gulp.task('Build Xplug', function() {
 
@@ -16,14 +18,40 @@ gulp.task('Build Xplug', function() {
               'source/xplug_storage.js',
               'source/xplug_menu.js',
               'source/xplug_powerbox.js',
-              'source/xplug_configure.js',              
+              'source/xplug_configure.js',
               'source/main.js'
             ])
-         .pipe(jshint())
-         .pipe(jshint.reporter('default'))
-         .pipe(concat('xplug.js'))
+
+        ///////////////////////////////////////////////////////////////////////
+        // Build Google Chrome add-on
+        ///////////////////////////////////////////////////////////////////////
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('xplug.js'))
+         ///////////////////////////////////////////////////////////////////////
+         // Build Google Chrome add-on
+         ///////////////////////////////////////////////////////////////////////
+        .pipe(header('// Built using Gulp. Built date: ${date}\n', {date : df() }))
+        .pipe(gulp.dest('xplug_chrome_plugin/js'))
+         //
+         ///////////////////////////////////////////////////////////////////////
+         // Special treatment of Firefox version of Xplug add-on
+         ///////////////////////////////////////////////////////////////////////
+         //
+         // Strip all comments. Firefox human reviewers don't like comments.
+         // Based on review feedback of Xplug v1.3.0.2 on 04-07-2016
+         //
+         .pipe(strip_comments())
+         //
+         // Strip all console statements. Firefox human reviewers don't like
+         // console logging in production add-ons.
+         // Based on review feedback of Xplug v1.3.0.2 on 04-07-2016
+         //
+         .pipe(strip_debug())
+         ///////////////////////////////////////////////////////////////////////
+         // Build Firefox add-on
+         ///////////////////////////////////////////////////////////////////////
          .pipe(header('// Built using Gulp. Built date: ${date}\n', {date : df() }))
-         .pipe(gulp.dest('xplug_chrome_plugin/js'))
          .pipe(gulp.dest('xplug_firefox_plugin/ext/js'));
 
 });
